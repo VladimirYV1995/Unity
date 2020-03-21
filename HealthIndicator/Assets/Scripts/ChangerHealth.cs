7365473e84c01aec169b65fc7af8bdd51798b129
Vladimir Yurin <v.yurin.jr@mail.ru>
2020-03-21 13:51:19 +0300

    Adding HealthIndicator

---
+++ HealthIndicator/Assets/Scripts/ChangerHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangerHealth : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private float _countChangeHealth = 10;
+    [SerializeField] private float _countSeconds = 2;
+
+    public void IncreaseHealth()
+    {
+        StartCoroutine(ChangeHealth(1));
+    }
+
+    public void DecreaseHealth()
+    {
+        StartCoroutine(ChangeHealth(-1));
+    }
+
+    private IEnumerator ChangeHealth(int direction)
+    {
+        float valueChangeHealth = direction * _countChangeHealth;
+        float nextSliderValue = _slider.value + valueChangeHealth;
+
+        if (nextSliderValue >= _slider.minValue && nextSliderValue <= _slider.maxValue)
+        {
+            if (_countSeconds <= 0)
+            {
+                _countSeconds = 1;
+            }
+
+            float speedChangeHealth = valueChangeHealth * Time.deltaTime / _countSeconds;
+
+            while (direction *_slider.value < direction * nextSliderValue)
+            {
+                _slider.value += speedChangeHealth ;
+                yield return null;
+            }
+        }
+        StopCoroutine(ChangeHealth(direction));
+    }
+}
